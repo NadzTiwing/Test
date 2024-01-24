9c9686de0336a10762ffe3473ce6bf5f31777d40
Nadia Tiwing <tiwingnadia@gmail.com>
2024-01-24 16:30:27 +0800

    update main file

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ console.log(`The sum of 1 and 2 is ${sum}`);
 
 const diff = operator.subtract(10,4);
 console.log(`The difference of 10 and 4 is ${diff}`);
+
+const product = operator.multiply(1,4);
+console.log(`The difference of 1 and 4 is ${product}`);
