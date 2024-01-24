cf0fa857db1841051b60f5ba744044d7fc7121fa
Nadia Tiwing <tiwingnadia@gmail.com>
2024-01-24 15:08:20 +0800

    add subtraction feature

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const operator = require('./feature1');
+const operator = require('./features');
 
 const sum = operator.add(1,2);
 console.log(`The sum of 1 and 2 is ${sum}`);
+
+const diff = operator.subtract(10,4);
+console.log(`The difference of 10 and 4 is ${diff}`);
