e122b0c9d6a0978cff11525e821f886458187f95
Nadia Tiwing <tiwingnadia@gmail.com>
2024-01-24 15:30:03 +0800

    add multiplication feature

--- features.js
+++ features.js
@@ -6,7 +6,12 @@ function subtract (num1, num2) {
     return num1 - num2;
 }
 
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
 module.exports = {
     add,
-    subtract
+    subtract,
+    multiply
 }
